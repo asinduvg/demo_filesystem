27b4a5c519a2b652c8dba40229f783a33355c997
asindu_vg <ashvg.dev@gmail.com>
2020-07-12 20:04:44 +0530

    mkdir created

--- src/com/rtjvm/scala/oop/files/Directory.scala
+++ src/com/rtjvm/scala/oop/files/Directory.scala
@@ -3,6 +3,23 @@ package com.rtjvm.scala.oop.files
 class Directory(override val parentPath: String, override val name: String, val contents: List[DirEntry])
   extends DirEntry(parentPath, name) {
 
+  def hasEntry(name: String): Boolean = ???
+
+  def getAllFoldersInPath: List[String] =
+    path.substring(1).split(Directory.SEPARATOR).toList
+
+  // /a/b/c/d => List["a", "b", "c", "d"]
+
+  def findDescendant(path: List[String]): Directory = ???
+
+  def addEntry(newEntry: DirEntry): Directory = ???
+
+  def findEntry(entryName: String): DirEntry = ???
+
+  def replaceEntry(entryName: String, newEntry: DirEntry): Directory = ???
+
+  override def asDirectory: Directory = this
+
 }
 
 object Directory {
@@ -11,6 +28,6 @@ object Directory {
 
   def ROOT: Directory = Directory.empty("", "")
 
-  private def empty(parentPath: String, name: String): Directory = //remove private
+  def empty(parentPath: String, name: String): Directory =
     new Directory(parentPath, name, List())
 }
